4cfbc26fad399aa4695bdf0e18a94eee811f1da7
SanskarGosavi2003 <210100132@iitb.ac.in>
2023-04-12 00:22:46 +0530

    First Commit Setup Vue app along with router and json-server for a temporary backend and npm-run-all Add: scripts 'server' to run vite app as well as json server in parallel using npm-run-all

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 8000,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/')
+      },
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
