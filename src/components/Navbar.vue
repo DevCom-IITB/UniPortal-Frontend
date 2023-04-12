ebe51529186ba00051be00d5f34211d2e1ed0540
ananyac9 <ananyachavadhal@gmail.com>
2023-04-12 20:45:13 +0530

    added Navbar and Sidebar components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="navbar">
+        <h1>Infopost</h1>
+        <h1>Questions</h1>
+        <h1>My Questions</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped>
+h1 {
+    background: #CCCCCC;
+}
+.navbar {
+background: #CCCCCC;
+color: #000000;
+font-family: Inter;
+display: flex;
+justify-content: space-between;
+align-items: center;
+margin-left: 664px;
+margin-top: 24px;
+height: 88px;
+width: 1018px;
+border-radius: 52px;
+padding: 25px 48px 25px 48px;
+font-weight:400;
+position: absolute;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="sidebar"></div> 
+</template>
+
+<script>
+export default {
+    name: 'Sidebar'
+}
+</script>
+
+<style scoped>
+.sidebar{
+    background: #d9d9d9;
+    color: #000000;
+    margin-left: 0px;
+    margin-top: 0px;
+    width: 404px;
+    height: 100vh;
+    position: absolute;
+
+}
+</style>
